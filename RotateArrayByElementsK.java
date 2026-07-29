701392ef5dea7f64cfac4d62fd70811289a329bb
Somya <somyatanwar1978@gmail.com>
2026-07-29 23:10:33 +0530

    useles comit 4

--- RotateArrayByElementsK.java
+++ RotateArrayByElementsK.java
@@ -28,4 +28,7 @@ class RotateArrayByElementsK{
            reverseArray(arr, 0, k-1);
            reverseArray(arr, n-1, k-1);
            reverseArray(arr, 0, n-1);
- 
+        }
+        return arr;
+    }
+}
