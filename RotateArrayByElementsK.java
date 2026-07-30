a9c4d4cf85556fdbad1bfd6f1d3a54c40e74a0f9
Somya <somyatanwar1978@gmail.com>
2026-07-30 23:08:37 +0530

    c3

--- RotateArrayByElementsK.java
+++ RotateArrayByElementsK.java
@@ -24,9 +24,5 @@ class RotateArrayByElementsK{
            reverseArray(arr,0 , k-1);
            reverseArray(arr, n-1, k-1);
         }
-        else if(direction.equals("left")){
-           reverseArray(arr, 0, k-1);
-           reverseArray(arr, n-1, k-1);
-           reverseArray(arr, 0, n-1);
-        }
+ 
    
