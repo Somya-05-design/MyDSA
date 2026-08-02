333dc0fede46cab644d1d4c616c142d4657132e7
Somya <somyatanwar1978@gmail.com>
2026-08-02 23:13:49 +0530

    cm1

--- FunctionsArr.java
+++ FunctionsArr.java
@@ -6,20 +6,3 @@ public class FunctionsArr{
         int arr[] = new int[n] ;
         Scanner scanner = new Scanner (System.in);
 
-        for (int i = 0 ; i < n ; i++){
-            System.out.print("Enter the value for INDEX " + i + ": ");
-            arr[i] = scanner.nextInt();
-        }
-        for (int i = 0 ; i < n ; i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the length of Array: ");
-       int n = scanner.nextInt();
-       Array(n);
-       scanner.close();
-    }
-}
