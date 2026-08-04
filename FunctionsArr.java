5212963b28ad31770e64c3e304bd10c8b78f4b28
Somya <somyatanwar1978@gmail.com>
2026-08-04 22:58:07 +0530

    qwertyuio

--- FunctionsArr.java
+++ FunctionsArr.java
@@ -15,3 +15,11 @@ public class FunctionsArr{
         }
         System.out.println();
     }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of Array: ");
+       int n = scanner.nextInt();
+       Array(n);
+       scanner.close();
+    }
+}
