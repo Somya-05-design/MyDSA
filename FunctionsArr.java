d66e7db8f75a7b53ac9239b76f456408acc67260
Somya <somyatanwar1978@gmail.com>
2026-08-08 21:01:44 +0530

    saza

--- FunctionsArr.java
+++ FunctionsArr.java
@@ -15,3 +15,7 @@ public class FunctionsArr{
         }
         System.out.println();
     }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of Array: ");
+ 
