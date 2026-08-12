7214e07f6d4eb935390ceafe05fc1886c9b13820
Somya <somyatanwar1978@gmail.com>
2026-08-12 23:38:25 +0530

    assax

--- CheckForSortedArray.java
+++ CheckForSortedArray.java
@@ -30,9 +30,4 @@ class CheckForSortedArray{
         System.out.println("The user Inputted Array is : " +Arrays.toString(numbers));
         boolean result = issorted(numbers);
         
-        if(result){
-            System.out.println("Array is sorted");
-        }
-        else{
-            System.out.println("Array is not sorted");
-    }
+ 
