01085ed8162a4791b79ec163ff882b9c3f4b54de
Somya <somyatanwar1978@gmail.com>
2026-08-14 23:18:10 +0530

    sout fxn

--- Functions.java
+++ Functions.java
@@ -9,9 +9,3 @@ public class Functions{
      int num1, num2;
 Scanner scanner = new Scanner(System.in);
 
-System.out.print("Enter num1 : ");
-num1 = scanner.nextInt();
-System.out.print("Enter num2 : ");
-num2 = scanner.nextInt();
-int func = sum(num1, num2);
-
